841c4917357cba2ad338864fc2e9111b6497d8a0
Tristan <tsilvas0113@gmail.com>
2022-07-19 13:47:47 -0500

    Allow to go to game screen with button click

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React from "react";
 
 function App() {
+
+  const [gameStart, setGameStart] = React.useState(false)
+
+  function startNewGame() {
+    setGameStart(!gameStart)
+  }
+
   return (
     <main>
+    {
+      gameStart
+      ?
+      <div>
+        <h1>Game Screen</h1>
+      </div>
+      :
       <div className='start-screen'>
         <h1 className='start-screen--title'>Quizzical</h1>
         <h1 className='start-screen--title'>Entertainment: Video Games</h1>
         <p className='start-screen--description'>Hello! Welcome to my Quizzical game: Video Games Edition. Start the quiz and try to correctly answer 10 multiple choice questions about video games. Good luck!</p>
-        <button className='start-screen--btn'>Start quiz</button>
+        <button 
+          className='start-screen--btn'
+          onClick={startNewGame}
+          >Start quiz
+        </button>
       </div>
+    }
     </main>
   );
 }
