1a75bca56dcec0f8444234aec80f71ab8d88ce89
Tristan <tsilvas0113@gmail.com>
2022-07-19 11:47:14 -0500

    Create and style start screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div>
-    </div>
+    <main>
+      <div className='start-screen'>
+        <h1 className='start-screen--title'>Quizzical</h1>
+        <h1 className='start-screen--title'>Entertainment: Video Games</h1>
+        <p className='start-screen--description'>Hello! Welcome to my Quizzical game: Video Games Edition. Start the quiz and try to correctly answer 10 multiple choice questions about video games. Good luck!</p>
+        <button className='start-screen--btn'>Start quiz</button>
+      </div>
+    </main>
   );
 }
 
