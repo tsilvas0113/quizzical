0651c6e0bc493896bf10834abb6dcbed3f19845a
Tristan <tsilvas0113@gmail.com>
2022-07-20 17:26:24 -0500

    Remove display of encoding on questions/answers

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -3,15 +3,18 @@ import { nanoid } from "nanoid";
 
 function Quiz(props) {
 
+    const regex = /(&quot;)|(&#039;t)|(&#039;s)|(&#039;)|(&amp;!)|(&amp;)|(&uuml;)/gi;
+    const otherRegex = /&eacute;/gi;
+
     const answerElements = props.answers.map(item =>{
         return (
-            <button key={nanoid()}>{item}</button>
+            <button key={nanoid()}>{item.replace(regex, "").replace(otherRegex, "e")}</button>
         )    
     })
 
     return (
         <div className="quiz">
-            <div className="quiz--questions">{props.question}</div>
+            <div className="quiz--questions">{props.question.replace(regex, "").replace(otherRegex, "e")}</div>
             <div className="quiz--answers">
                 {answerElements}
             </div>
